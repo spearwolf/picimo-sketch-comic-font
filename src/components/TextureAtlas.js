241d7164f569e06edbaf7a0e0b69b7f01215117c
Wolfger Schramm <wolfger@spearwolf.de>
2019-11-04 22:47:51 +0100

    improve <TextureAtlas>

--- src/components/TextureAtlas.js
+++ src/components/TextureAtlas.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import {extend} from 'react-three-fiber';
 import usePromise from 'react-promise-suspense';
-import {TextureAtlas as PicimoTextureAtlas, Texture, PowerOf2Image} from 'picimo';
+import {TextureAtlas as PicimoTextureAtlas} from 'picimo';
 import {string} from 'prop-types';
-
-extend({PicimoTextureAtlas});
 
 export const TextureAtlas = ({src, basePath, attach}) => {
 
-  const args = usePromise(async () => {
-    const atlas = await fetch(`${basePath}${src}`).then(response => response.json());
-    const image = await new PowerOf2Image(`${basePath}${atlas.meta.image}`).loaded;
-    const baseTexture = new Texture(image);
-    return [baseTexture, atlas];
-  }, [src, basePath]);
+  const textureAtlas = usePromise(
+    () => PicimoTextureAtlas.load(src, basePath),
+    [src, basePath],
+  );
 
-  return <picimoTextureAtlas args={args} attach={attach}></picimoTextureAtlas>;
+  return <primitive object={textureAtlas} attach={attach}></primitive>;
 
 }
 
