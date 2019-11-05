ea2cc14eae147d1d372350eca739b0613ae10ea8
Wolfger Schramm <wolfger@spearwolf.de>
2019-11-05 08:15:08 +0100

    forward all unknown props tpo TextureAtlas

--- src/components/TextureAtlas.js
+++ src/components/TextureAtlas.js
@@ -3,21 +3,20 @@ import usePromise from 'react-promise-suspense';
 import {TextureAtlas as PicimoTextureAtlas} from 'picimo';
 import {string} from 'prop-types';
 
-export const TextureAtlas = ({src, basePath, attach}) => {
+export const TextureAtlas = ({src, basePath, ...props}) => {
 
   const textureAtlas = usePromise(
     () => PicimoTextureAtlas.load(src, basePath),
     [src, basePath],
   );
 
-  return <primitive object={textureAtlas} attach={attach}></primitive>;
+  return <primitive object={textureAtlas} {...props}></primitive>;
 
 }
 
 TextureAtlas.propTypes = {
   src: string.isRequired,
   basePath: string,
-  attach: string.isRequired,
 }
 
 TextureAtlas.defaultProps = {
